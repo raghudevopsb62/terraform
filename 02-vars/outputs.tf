51bc610198b4bce6a847535d9fbc94e50c3aa82b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-21 08:26:27 +0530

    Update terraform/

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -2,3 +2,6 @@ output "sample" {
   value = var.sample
 }
 
+output "list1" {
+  value = var.list[0]
+}
--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,24 @@ variable "sample" {
 }
 
 
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+## Single Quotes are not supported in Terraform
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+variable "list" {
+  default = ["Hello", 90, true]
+}
+# List can have values of different data types.
