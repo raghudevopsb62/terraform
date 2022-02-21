4af69559d00062a20e6fa18710172b32e0792272
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-21 08:54:44 +0530

    Update terraform/

--- examples/01-sg-with-ec2/ec2.tf
+++ examples/01-sg-with-ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0d997c5f64a74852c"
-  instance_type = "t3.micro"
+  ami                    = "ami-0d997c5f64a74852c"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "web"
