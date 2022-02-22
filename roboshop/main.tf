21d45ef9150fee8762f7007e5db4fce58a2c6120
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-22 09:10:51 +0530

    Update terraform/

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,31 +1,39 @@
-module "frontend" {
-  source = "./frontend"
-}
-module "cart" {
-  source = "./cart"
-}
-module "catalogue" {
-  source = "./catalogue"
-}
-module "payment" {
-  source = "./payment"
-}
-module "shipping" {
-  source = "./shipping"
-}
-module "mysql" {
-  source = "./mysql"
-}
-module "mongodb" {
-  source = "./mongodb"
-}
-module "redis" {
-  source = "./redis"
-}
-module "rabbitmq" {
-  source = "./rabbitmq"
+//module "frontend" {
+//  source = "./frontend"
+//}
+//module "cart" {
+//  source = "./cart"
+//}
+//module "catalogue" {
+//  source = "./catalogue"
+//}
+//module "payment" {
+//  source = "./payment"
+//}
+//module "shipping" {
+//  source = "./shipping"
+//}
+//module "mysql" {
+//  source = "./mysql"
+//}
+//module "mongodb" {
+//  source = "./mongodb"
+//}
+//module "redis" {
+//  source = "./redis"
+//}
+//module "rabbitmq" {
+//  source = "./rabbitmq"
+//}
+//
+//module "user" {
+//  source = "./user"
+//}
+
+module "roboshop" {
+  source    = "./module"
+  COMPONENT = var.COMPONENTS
+  PORT      = var.PORTS
+  MONITOR   = var.MONITOR
 }
 
-module "user" {
-  source = "./user"
-}
