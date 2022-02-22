39addd0ce1651bebc3c93fd522d832234c7adb34
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-22 08:24:00 +0530

    Update terraform/

--- roboshop/module/ec2.tf
+++ roboshop/module/ec2.tf
@@ -12,6 +12,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                    = "ami-0d997c5f64a74852c"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_app.id]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = var.COMPONENT
