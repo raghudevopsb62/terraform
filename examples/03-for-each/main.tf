1ac3126cd857b34332f60cb021eb0000bbeefaff
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-23 08:14:15 +0530

    Update terraform/

---
+++ examples/03-for-each/main.tf
@@ -0,0 +1,31 @@
+variable "COMP" {
+  default = [
+    {
+      name    = "frontend"
+      port    = 80
+      monitor = "yes"
+    },
+    {
+      name    = "catalogue"
+      port    = 8080
+      monitor = "yes"
+    }
+  ]
+}
+
+//resource "null_resource" "null" {
+//  for_each = var.COMP
+//  triggers = {
+//    abc = timestamp()
+//  }
+//  provisioner "local-exec" {
+//    command = "Component Name = ${each.value["name"]}"
+//  }
+//}
+
+
+module "sample" {
+  count  = length(var.COMP)
+  source = "./module"
+  comp   = element(var.COMP, count.index)
+}
---
+++ examples/03-for-each/module/vars.tf
@@ -0,0 +1,5 @@
+variable "comp" {
+  type = map(string)
+}
+
+
